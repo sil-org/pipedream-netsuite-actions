ea2d929732a0757f9a66ac4e879f528078ebc859
Matt H. <matt_henderson@sil.org>
2026-02-03 16:46:44 -0500

    Limit CI/CD tests to those unlikely to trigger errors on live systems
    See the README.md file for details.

--- exchange_rate_lookup.test.js
+++ exchange_rate_lookup.test.js
@@ -4,8 +4,8 @@ import { format } from 'node:util'
 
 const { default: component } = await import('./exchange_rate_lookup.js')
 
-describe('Exchange Rate Lookup', () => {
-  it('should get the expected value for a specific transaction', async (testContext) => {
+describe.only('Exchange Rate Lookup', () => {
+  it.only('should get the expected value for a specific transaction', async (testContext) => {
     if (!process.env.NETSUITE_CONFIG_DEV) {
       testContext.skip('No NETSUITE_CONFIG_DEV env. var. found, so skipping integration test.')
       return
