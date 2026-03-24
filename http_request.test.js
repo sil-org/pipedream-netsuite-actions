698fef4c82194516b83c08af8bfbe23725b30a8c
Matt H. <matt_henderson@sil.org>
2026-03-24 16:49:11 -0400

    Add test for NetSuite Request (re: returning error data, not throwing)

---
+++ http_request.test.js
@@ -0,0 +1,44 @@
+import assert from 'node:assert/strict'
+import { loadEnvFile } from 'node:process'
+import { describe, it } from 'node:test'
+
+try {
+  loadEnvFile('.env')
+} catch (error) {
+  if (error.code === 'ENOENT') {
+    console.log('No env file found, proceeding without it')
+  } else {
+    throw error
+  }
+}
+
+const { default: component } = await import('./http_request.js')
+
+describe(component.name, () => {
+  it('should return error data (not throw an Error)', async (testContext) => {
+    if (!process.env.NETSUITE_CONFIG_DEV) {
+      testContext.skip('No NETSUITE_CONFIG_DEV env. var. found, so skipping integration test.')
+      return
+    }
+    component.config = process.env.NETSUITE_CONFIG_DEV
+    component.method = 'GET'
+    component.path = '/record/v1/customer/invalid-id-999'
+    component.headers = {}
+    component.get_more = false
+    component.skip = false
+    try {
+      const response = await component.run({
+        steps: {trigger: {}},
+        $: {
+          export: console.log
+        }
+      })
+    } catch (error) {
+      assert.fail(`An error was thrown (${error.message || error})`)
+    }
+    assert.ok(
+      response.error.length > 0,
+      'Did not find the expected error property.'
+    )
+  })
+})
