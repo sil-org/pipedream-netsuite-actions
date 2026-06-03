8dd39f1c33fbd802cbcaa87354a37f1085fadf79
Jason Jackson <jasonc.jackson@gmail.com>
2026-06-03 11:39:09 -0400

    exchange_rate_lookup use pipedream/utils

--- exchange_rate_lookup.js
+++ exchange_rate_lookup.js
@@ -1,23 +1,8 @@
+import { netsuite } from '@sil-org/pipedream-utils@^0.2.0'
 import assert from 'node:assert/strict'
 import { format } from 'node:util'
-import { NetsuiteApiClient } from 'netsuite-api-client@^1.0.3'
 
 export const methods = {
-  handleTimeout(start, count, timeout, timeoutRecords) {
-    const duration = Date.now() - start
-    if (timeout && duration >= timeout * 1000) {
-      console.error(`Timeout reached at ${duration / 1000} seconds`)
-      return true
-    }
-
-    if (timeoutRecords && count >= timeoutRecords) {
-      console.error(`Timeout reached at ${count} records in ${duration / 1000} seconds`)
-      return true
-    }
-
-    return false
-  },
-
   /**
    * Convert the given date string to a Date-Only ISO-8601 date string (YYYY-MM-DD).
    * @param {string} dateString
@@ -30,82 +15,6 @@ export const methods = {
   },
 }
 
-const queryRecords = {
-  name: "NetSuite Query Records",
-  description: "Run a SuiteQL query against NetSuite records.",
-  key: "netsuite_query_records",
-  version: "0.1.0",
-  type: "action",
-
-  props: {
-    config: {
-      type: "string",
-      label: "NetSuite Config",
-      description: "Configuration object returned from the initialization step.",
-      secret: true,
-    },
-    query: {
-      type: "string",
-      label: "SuiteQL Query",
-      description:
-        "Enter a SuiteQL query, e.g. `SELECT id, entityid, email FROM customer WHERE isinactive = 'F' LIMIT 10`",
-    },
-    timeout: {
-      type: "integer",
-      label: "Timeout in Seconds",
-      description: "The timeout in seconds",
-      min: 0,
-      optional: true,
-    },
-    timeout_records: {
-      type: "integer",
-      label: "Limit",
-      description: "The maximum number of records to return before timing out.",
-      min: 1,
-      default: 1000,
-      optional: true,
-    },
-  },
-
-  async run({ $ }) {
-    const client = new NetsuiteApiClient(JSON.parse(this.config));
-
-    const limit = Math.min(1000, this.timeout_records)
-    let offset = 0
-    const start = Date.now()
-    let response = {}
-
-    try {
-      let items = []
-      do {
-        response = await client.query(this.query, limit, offset)
-        items = items.concat(response.items)
-        offset += limit
-
-        if (methods.handleTimeout(start, items.length, this.timeout, this.timeout_records)) {
-          break
-        }
-      } while (response.hasMore)
-
-      $.export(
-        "$summary",
-        `Successfully ran SuiteQL query`
-      );
-      return items;
-    } catch (error) {
-      console.error(
-        "NetSuite API Error:",
-        error.response?.data || error.message
-      );
-      throw new Error(
-        `Failed to execute SuiteQL query: ${
-          error.response?.data?.detail || error.message
-        }`
-      );
-    }
-  },
-};
-
 export default {
   name: "Exchange Rate Lookup (NetSuite)",
   description: "Retrieve a specific currency exchange rate from NetSuite",
@@ -113,7 +22,7 @@ export default {
   type: "action",
 
   // Unless urgently needed, this action should only be modified in Github
-  version: "0.2.1",
+  version: "0.2.2",
 
   props: {
     netsuite_config_json: {
@@ -150,11 +59,10 @@ export default {
 
   async run({ steps, $ }) {
     const transactionDateForQuery = methods.toDateOnlyISO8601(this.transaction_date)
-    queryRecords.config = JSON.parse(this.netsuite_config_json)
-    console.log("Realm:", queryRecords.config.realm)
+    const netsuiteConfig = JSON.parse(this.netsuite_config_json)
+    console.log("Realm:", netsuiteConfig.realm)
 
-    queryRecords.timeout_records = 1000
-    queryRecords.query = `
+    const query = `
         SELECT
             exchangerate
         FROM
@@ -166,7 +74,7 @@ export default {
     `;
 
     try {
-      const results = await queryRecords.run({steps, $})
+      const results = await netsuite.queryRecords(query, netsuiteConfig)
       assert.ok(
         results.length > 0,
         'No matching Currency Rate in NetSuite'
